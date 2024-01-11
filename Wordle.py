39f87d0215212df14897632e10b52f9c941191db
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-01-10 21:40:12 -0700

    Jake - see previous comment

--- Wordle.py
+++ Wordle.py
@@ -17,10 +17,10 @@ def wordle():
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
-    #MILESTONE 1
-    for row in range(0, N_ROWS):
-        for col, letter in enumerate(word_of_the_day):
-            gw.set_square_letter(row, col, letter)
+    # #MILESTONE 1
+    # for row in range(0, N_ROWS):
+    #     for col, letter in enumerate(word_of_the_day):
+    #         gw.set_square_letter(row, col, letter)
 
 # Startup code
 if __name__ == "__main__":
