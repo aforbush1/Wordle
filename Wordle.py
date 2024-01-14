1950e924c13f378ee3ed7057e47a0e2eff01fd20
aforbush1 <aforbush17@gmail.com>
2024-01-13 17:48:17 -0700

    Adam

--- Wordle.py
+++ Wordle.py
@@ -10,10 +10,11 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT
 def wordle():
     # Define the maximum number of guesses
     MAX_GUESSES = 6
-    current_row = 0
+    
     # Create the WordleGWindow object
     gw = WordleGWindow()
 
+    current_row = gw.get_current_row()
     # Random select from the Array of 5 letter words, set to variable "word_of_the_day".
     word_of_the_day = random.choice(FIVE_LETTER_WORDS).lower()
     print(word_of_the_day)
@@ -27,26 +28,29 @@ def wordle():
             return
 
         for col, letter in enumerate(guess):
-            gw.set_square_letter(0, col, letter)
+            gw.set_square_letter(current_row, col, letter)
 
         if(guess.lower() in FIVE_LETTER_WORDS):
             if(guess.lower() == word_of_the_day.lower()):
                 for col in range(0, N_COLS):
                     # row needs to be adjusted to be current
-                    gw.set_square_color(0, col, CORRECT_COLOR)
+                    gw.set_square_color(current_row, col, CORRECT_COLOR)
                 gw.show_message("You guessed the word!")
             else:
                 for col in range(0, N_COLS):
-                    if(gw.get_square_letter(0, col).lower() == word_of_the_day[col]):
-                        gw.set_square_color(0, col, CORRECT_COLOR)
-                    elif(gw.get_square_letter(0, col).lower() in word_of_the_day):
-                        gw.set_square_color(0, col, PRESENT_COLOR)
+                    if(gw.get_square_letter(current_row, col).lower() == word_of_the_day[col]):
+                        gw.set_square_color(current_row, col, CORRECT_COLOR)
+                    elif(gw.get_square_letter(current_row, col).lower() in word_of_the_day):
+                        gw.set_square_color(current_row, col, PRESENT_COLOR)
                     else:
-                        gw.set_square_color(0, col, MISSING_COLOR)
+                        gw.set_square_color(current_row, col, MISSING_COLOR)
+                current_row += 1
+
         elif(guess.lower() not in FIVE_LETTER_WORDS):
             gw.show_message("Not in word list")
             current_row += 1
 
+        gw.set_current_row(current_row)
     # Set the enter_action function as a callback for the ENTER key
     # gw.set_square_letter(0, 0, 'H')
     gw.add_enter_listener(enter_action)
