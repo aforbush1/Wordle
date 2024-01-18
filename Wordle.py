f67a60c7464dd8b54ac2e8580543fffc583d6559
aforbush1 <aforbush17@gmail.com>
2024-01-17 17:44:28 -0700

    Adam

--- Wordle.py
+++ Wordle.py
@@ -17,16 +17,17 @@ def wordle():
     # Set the get_current_row() function to variable
     current_row = gw.get_current_row()
     # Random select from the Array of 5 letter words, set to variable "word_of_the_day".
-    word_of_the_day = random.choice(FIVE_LETTER_WORDS).lower()
+    # word_of_the_day = random.choice(FIVE_LETTER_WORDS).lower()
+    word_of_the_day = 'World'
 
     # Function for [ENTER]
     def enter_action(guess):
         # Call global variable
         nonlocal current_row
 
-        if current_row >= MAX_GUESSES:
-            gw.show_message(f"The word of the day is {word_of_the_day.upper()}")
-            return
+        # if current_row >= MAX_GUESSES:
+        #     gw.show_message(f"The word of the day is {word_of_the_day.upper()}")
+        #     return
         
         # Create local variables
         word = []
